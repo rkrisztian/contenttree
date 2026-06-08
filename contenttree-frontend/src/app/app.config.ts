fd185291ce630e36eec95811023c8851f4325a01
rkrisztian <rkrisztian@users.noreply.github.com>
2026-06-08 14:28:52 +0200

    refactor(frontend): use `provideAppInitializer` for loading the theme service

--- contenttree-frontend/src/app/app.config.ts
+++ contenttree-frontend/src/app/app.config.ts
@@ -1,14 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { errorInterceptor } from './core/error-interceptor';
+import { ThemeToggleService } from './core/theme-toggle/theme-toggle.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([errorInterceptor])),
+    provideAppInitializer(() => {
+      inject(ThemeToggleService).initializeTheme();
+    }),
   ],
 };
--- contenttree-frontend/src/main.ts
+++ contenttree-frontend/src/main.ts
@@ -1,11 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
-import { ThemeToggleService } from './app/core/theme-toggle/theme-toggle.service';
 
-bootstrapApplication(App, appConfig)
-  .then((applicationRef) => {
-    const themeToggleService = applicationRef.injector.get(ThemeToggleService);
-    themeToggleService.initializeTheme();
-  })
-  .catch((err) => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
