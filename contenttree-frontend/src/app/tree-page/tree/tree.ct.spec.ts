71623c36f03c9db22e64d62397666b39f7079668
rkrisztian <rkrisztian@users.noreply.github.com>
2026-07-09 14:20:52 +0200

    test(frontend): use accessibility attribute to check for selected nodes

--- contenttree-frontend/src/app/tree-page/tree/tree.ct.spec.ts
+++ contenttree-frontend/src/app/tree-page/tree/tree.ct.spec.ts
@@ -8,14 +8,12 @@ import { TreePage } from '../tree-page';
 import { TreePageService } from '../tree-page.service';
 
 describe('Tree', () => {
-  let treePageService: TreePageService;
+  const grandChildNode = page.getByRole('button', { name: 'Grandchild node', exact: true });
 
   beforeEach(async () => {
     await render(TreePage, {
       providers: [TreePageService, TreeApiService],
     });
-
-    treePageService = TestBed.inject(TreePageService);
   });
 
   it('can display nodes', async () => {
@@ -36,24 +34,20 @@ describe('Tree', () => {
   });
 
   it('can select a node', async () => {
-    await page.getByRole('button', { name: 'Grandchild node', exact: true }).click();
+    await grandChildNode.click();
 
-    // Found no simple way to check for text color change,
-    // and the content is displayed by a separate component.
-    expect(treePageService.selectedNode()).toMatchObject({ id: 4 });
+    await expect.element(grandChildNode).toHaveAttribute('aria-selected', 'true');
   });
 
   it('can deselect a node', async () => {
-    await page.getByRole('button', { name: 'Grandchild node', exact: true }).click();
-    await page.getByRole('button', { name: 'Grandchild node', exact: true }).click();
+    await grandChildNode.click();
+    await grandChildNode.click();
 
-    expect(treePageService.selectedNode()).toBeNull();
+    await expect.element(grandChildNode).not.toHaveAttribute('aria-selected', 'true');
   });
 
   it('can move nodes', async () => {
-    await page
-      .getByRole('button', { name: 'Grandchild node', exact: true })
-      .dropTo(page.getByRole('button', { name: 'Root node', exact: true }));
+    await grandChildNode.dropTo(page.getByRole('button', { name: 'Root node', exact: true }));
     await TestBed.inject(ApplicationRef).whenStable();
 
     for (const name of ['Root node', 'Child node', 'Grandchild node', 'Child node 2']) {
