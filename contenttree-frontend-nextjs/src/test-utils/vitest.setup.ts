0c119bbf4fce4783ac0c08dd4ec53b76269b94c7
rkrisztian <rkrisztian@users.noreply.github.com>
2026-07-23 18:50:59 +0200

    chore(frontend-nextjs): eliminate race conditions in React hook tests

--- contenttree-frontend-nextjs/src/test-utils/vitest.setup.ts
+++ contenttree-frontend-nextjs/src/test-utils/vitest.setup.ts
@@ -1,7 +1,25 @@
+import { afterEach } from "node:test";
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
-import { afterEach, expect } from "vitest";
+import { afterAll, beforeAll, expect, vi } from "vitest";
 
 expect.extend(matchers);
 
-afterEach(() => cleanup());
+declare global {
+  var IS_REACT_ACT_ENVIRONMENT: boolean;
+}
+
+beforeAll(() => {
+  // Prevents race conditions (React states updates leaking across tests).
+  // See issue: https://github.com/testing-library/react-testing-library/issues/1413
+  globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+});
+
+afterEach(() => {
+  cleanup();
+});
+
+afterAll(() => {
+  // Prevent race conditions (reset module cache so each test gets fresh provider/context instances).
+  vi.resetModules();
+});
